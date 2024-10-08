7552ce2d30eed6c49816c84d969773e069da05fa
Rudolf <rudolf.chen@sigknow.com.tw>
2024-10-08 15:27:19 +0800

    feat: add day37

---
+++ day37-Habit Tracker Pixela/main.py
@@ -0,0 +1,34 @@
+import requests
+import strftime
+
+PIXELA_ENDPOINT = "https://pixe.la"
+PIXELA_TOKEN = "ihY7uc6W5Ips3H0mEuezaksUI77lD4PsrOGyXhUWdMrhyqyB2DB5zy3f3RoZ3Avp"
+PIXELA_USERNAME = "rudd"
+
+user_params = {
+    "token": PIXELA_TOKEN,
+    "username": PIXELA_USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=f"{PIXELA_ENDPOINT}/v1/users", json=user_params)
+# print(response.json())
+
+
+headers = {
+    "X-USER-TOKEN": PIXELA_TOKEN,
+}
+
+graph_params = {
+    "id": "graph-1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "shibafu"
+}
+
+response = requests.post(url=f"{PIXELA_ENDPOINT}/v1/users/{PIXELA_USERNAME}/graphs", json=graph_params, headers=headers)
+print(response.text)
+
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
